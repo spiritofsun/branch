9e20edd8fcf13115fd951122332a0fb09643cc04
spritofsun <sjm0825man@naver.com>
2020-04-22 15:21:43 +0900

    feat 3, 5, 15

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-for i in range(100):
-	 if i%15==0:
-		print("feez buzz ")
-	elif i%3==0:
-		print("feez ")
+for i in range(1,100):
+	 if i%3==0:
+		print("feez")
 	elif i%5==0:
-		print("buzz ")
+		print("buzz")
+	elif i%15==0:
+		print("feezbuzz")
 	else: 
 	 print(i) 
